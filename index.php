20d2c3f7418ddc81ba2c160441562b423dce9411
Hisao Mase <mhna072523@gmail.com>
2026-03-02 16:07:50 +0900

    HTML、CSSの作成

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>タスク管理アプリ</title>
+</head>
+<body>
+    <header>
+        <h1>タスク管理アプリ</h1>
+    </header>
+    <div id="main">
+        <div id="left">
+            <nav id="menu">
+                <ul>
+                    <li><メニュー></li>
+                    <li><a href="">新規入力</a></li>
+                    <li><a href="">タスク一覧表</a></li>
+                    <li><a href="">検索</a></li>
+                    <li><a href="">カレンダー</a></li>
+                    <li><a href="">ユーザー</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div id="right">
+            <div id="outer">
+                <div>
+                    <p id="title">今日のタスク内容</p>
+                </div>
+                <div>
+                    <div id="content">a<br>a<br></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>© 2026 タスク管理アプリ</p>
+    </footer>
+</body>
+</html>
